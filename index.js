747dc6c681612419f1fd91af56a566a17fb1b2cc
Sharjeel <sharjeel.nust07@gmail.com>
2025-04-18 15:44:12 +0500

    SSL Certificate Included

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const limiter = require("./middleware/rateLimiter");
-const app = express();
 const studentRoutes = require("./routes/studentRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
+const app = express();
+
 //Apply rate limiter
 app.use(limiter);
+app.use(express.json());
 
 // CORS
 app.use(
@@ -18,16 +23,41 @@ app.use(
   })
 );
 
-app.use(express.json());
+// Load SSL certificate and key
+const privateKey = fs.readFileSync(
+  path.join(__dirname, "ssl", "key.pem"),
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  path.join(__dirname, "ssl", "cert.pem"),
+  "utf8"
+);
+
+const credentials = { key: privateKey, cert: certificate };
+//
+//
+//
+
 app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+//Routes
 app.use("/students", studentRoutes);
 app.use("/admins", adminRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
+// Create HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+// Run server
+httpsServer.listen(process.env.PORT, () => {
+  console.log(
+    `HTTPS Server is running on https://localhost:${process.env.PORT}`
+  );
 });
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server listening on port ${process.env.PORT}`);
+// });
